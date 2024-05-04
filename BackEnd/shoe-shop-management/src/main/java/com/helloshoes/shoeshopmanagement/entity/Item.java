d5558629c3c074df7ab17370f1b0b744bee62395
thisarakavishka <thisarakavishka0@gmail.com>
2024-05-05 03:05:44 +0530

    Refactor Item entity

--- BackEnd/shoe-shop-management/src/main/java/com/helloshoes/shoeshopmanagement/entity/Item.java
+++ BackEnd/shoe-shop-management/src/main/java/com/helloshoes/shoeshopmanagement/entity/Item.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 @Entity
 @Table(name = "item")
+@ToString(exclude = {"suppliers", "sizes", "categories", "types", "colours", "itemDetails"})
 public class Item implements SuperEntity {
     @Id
     private String itemCode;
