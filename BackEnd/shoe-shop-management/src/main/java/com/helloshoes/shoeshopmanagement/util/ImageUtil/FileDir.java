93e81778ebd4ce88a5c8479ec8e8f49585bfd038
thisarakavishka <thisarakavishka0@gmail.com>
2024-05-12 01:05:02 +0530

    Implement File saver util

---
+++ BackEnd/shoe-shop-management/src/main/java/com/helloshoes/shoeshopmanagement/util/ImageUtil/FileDir.java
@@ -0,0 +1,8 @@
+package com.helloshoes.shoeshopmanagement.util.ImageUtil;
+
+public class FileDir {
+    public static final String PARENT_FOLDER_PATH = "src/main/resources/static/images";
+    public static final String EMPLOYEE_FOLDER = "employee";
+    public static final String SHOE_FOLDER = "shoe";
+
+}
---
+++ BackEnd/shoe-shop-management/src/main/java/com/helloshoes/shoeshopmanagement/util/ImageUtil/FileUtil.java
@@ -0,0 +1,61 @@
+package com.helloshoes.shoeshopmanagement.util.ImageUtil;
+
+import java.io.*;
+import java.util.Base64;
+
+public class FileUtil {
+    public static String saveImageStringToFile(String base64String, String parentFolderPath, String dir, String fileName) {
+        String folderPath = parentFolderPath + File.separator + dir;
+
+        if (!createDirectory(parentFolderPath) || !createDirectory(folderPath)) {
+            return null;
+        }
+
+        byte[] imageBytes = base64StringToArray(base64String);
+
+        if (imageBytes == null) {
+            return null;
+        }
+
+        String filePath = folderPath + File.separator + fileName;
+
+        if (!writeImageByteToFile(imageBytes, filePath)) {
+            return null;
+        }
+        return filePath;
+    }
+
+    private static boolean writeImageByteToFile(byte[] imageBytes, String filePath) {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(filePath)) {
+            fileOutputStream.write(imageBytes);
+            System.out.println("Image successfully saved as: " + filePath);
+            return true;
+        } catch (IOException e) {
+            System.err.println("Error writing image file: " + e.getMessage());
+            return false;
+        }
+    }
+
+    private static byte[] base64StringToArray(String base64String) {
+        try {
+            return Base64.getDecoder().decode(base64String);
+        } catch (IllegalArgumentException e) {
+            System.err.println("Error decoding Base64 string: " + e.getMessage());
+            return null;
+        }
+    }
+
+    private static boolean createDirectory(String folderPath) {
+        File folder = new File(folderPath);
+        if (folder.exists()) {
+            return true;
+        }
+        if (folder.mkdirs()) {
+            System.out.println("Folder created: " + folderPath);
+            return true;
+        } else {
+            System.err.println("Error creating folder: " + folderPath);
+            return false;
+        }
+    }
+}
