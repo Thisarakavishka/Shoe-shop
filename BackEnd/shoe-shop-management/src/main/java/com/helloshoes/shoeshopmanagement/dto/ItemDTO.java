501e40a85fc28133fffaa7862e6ad243eda22832
thisarakavishka <thisarakavishka0@gmail.com>
2024-05-14 13:13:38 +0530

    Refactor Item DTOs

--- BackEnd/shoe-shop-management/src/main/java/com/helloshoes/shoeshopmanagement/dto/ItemDTO.java
+++ BackEnd/shoe-shop-management/src/main/java/com/helloshoes/shoeshopmanagement/dto/ItemDTO.java
@@ -11,6 +11,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ItemDTO implements SuperDTO{
+    private String itemCode;
     private String itemName;
     private String categoryName;
     private String supplierName;
